ac864765d2ce2aafc63c3be87e1e04d114d0c63b
KarlaBinder <karlabinder1@gmail.com>
2023-11-11 18:07:49 +0100

    Task 2

---
+++ RUAP-LV2/Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        // GET: api/contact
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string[] greetings = new string[]
+            {
+                "Hello",
+                "World"
+            };
+
+            return Ok(greetings);
+        }
+    
+    }
+}
